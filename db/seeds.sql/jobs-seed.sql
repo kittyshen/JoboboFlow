89817a12473738598482e23fdad23fa279fce385
Han Ban <hannah15198@gmail.com>
2018-06-11 13:28:48 -0700

    Added seed data for jobs model

---
+++ db/seeds.sql/jobs-seed.sql
@@ -0,0 +1,14 @@
+INSERT INTO jobs (jobLink, jobTitle, companyName, applied, phone_interview, site_interview, outcome)
+VALUES ("https://angel.co/bolt/jobs/310250-software-engineer-machine-learning", "Software Engineer - Machine Learning", "Bolt", 1, 0, 0, 0)
+
+INSERT INTO jobs (jobLink, jobTitle, companyName, applied, phone_interview, site_interview, outcome)
+VALUES ("https://angel.co/clearbit/jobs/362514-full-stack-software-engineer", "Full-stack software engineer", "Clearbit", 1);
+
+INSERT INTO jobs (jobLink, jobTitle, companyName, applied, phone_interview, site_interview, outcome)
+VALUES ("https://angel.co/joinhouseparty/jobs/185904-backend", "Backend ", "Houseparty", 1, 0, 0, 0 );
+
+INSERT INTO jobs (jobLink, jobTitle, companyName, applied, phone_interview, site_interview, outcome)
+VALUES ("https://angel.co/fathom-health/jobs/186407-software-engineer-data", "Software Engineer, Data", "Fathom", 1, 0, 0, 0);
+
+INSERT INTO jobs (jobLink, jobTitle, companyName, applied, phone_interview, site_interview, outcome)
+VALUES ("https://angel.co/crew-3/jobs/158070-front-end-developer", "Front-end Developer", "Crew", 1, 0, 0, 0);
