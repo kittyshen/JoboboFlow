b85f81533ed5eae9c7a1d6d0b6017d3ff1939d56
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-06-13 16:14:39 -0700

    Clean up previous demo of chaining .then() -remove unnecessary step

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -4,8 +4,7 @@ var router = express.Router();
 var path = require('path');
 var db = require(path.join(__dirname, '../models'));
 
-// ====================== Nick =========================
-router.post('/user/add', function (req, res) {
+router.post('/user/add', function(req, res) {
 
   var formData = {
     user_name: req.body.user_name,
@@ -16,49 +15,26 @@ router.post('/user/add', function (req, res) {
 
   // Query DB for the cohort by name (req.body.cohort_name)
   db.Cohort.findOne({ where: { cohort_name: req.body.cohort_name } })
-    .then(function (data) {
-      // get dataValues from the response Sequelize response object
+    .then(function(data) {
+      // get dataValues from the Sequelize response object
       var match = data.dataValues;
       // and take the id property from dataValues this is the cohortId
       var cohortId = match.id;
-      // Need to return the value here to be accessed in subsequent .then() calls
-      return cohortId;
-    })
-    .then(function (data) {
-      // data is cohortId. We are adding a property to the formData equal to cohortId
-      formData.CohortId = data;
+      // We are adding a property to the formData equal to cohortId
+      formData.CohortId = cohortId
       // create a user in the DB with their associated cohort ID
-      return db.User.create(formData)
+      return db.User.create(formData);
     })
     .then(function(data) {
       res.json(data);
     })
-    // Add a .catch method to the end of our promise chain to provide some
-    // error handling
-    .catch(function (err) {
-      console.error(err);
-    });
-
-  // return res.redirect('/user/'+id);
-  // res.render(path.join(__dirname,"../views/user.handlebars"));
-});
-// ======================================================
-
-router.post('/user/login', function (req, res) {
-  db.User.findAll({
-    where: { user_name: req.body.user_name }
-  })
-    .then(function(users) {
-      console.log(users.dataValues);
-      // users = users.dataValues;
-      // users.forEach(function(user) {
-      //   console.log(user);
-      // })
-    })
+      // Handle errors
     .catch(function(err) {
       console.error(err);
     });
+});
 
+router.post('/user/login', function(req, res) {
   res.json({ id: 3 });
   // return res.redirect('/user/'+id);
   // res.render(path.join(__dirname,"../views/user.handlebars"));
@@ -71,14 +47,14 @@ router.post('/user/login', function (req, res) {
 // }
 
 
-router.get('/user:id', function (req, res) {
+router.get('/user:id', function(req, res) {
   var id = req.params.id;
   // console.log(req.params.id + "kjljldjsf");
   // console.log('jon world! user show all user jobs');
   db.Job.findAll({
     include: [db.User],
     where: { UserId: id }
-  }).then(function (data) {
+  }).then(function(data) {
     res.render(path.join(__dirname, "../views/user.handlebars"), { jobs: data });
     // console.log("../views/user.handlebars");
     // res.render(path.join(__dirname,"../views/user.handlebars"),{jobs:data})
@@ -86,24 +62,42 @@ router.get('/user:id', function (req, res) {
   //   res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
-router.get('/user/:id/jobs', function (req, res) {
+router.get('/user/:id/jobs', function(req, res) {
   var id = req.params.id;
-  // console.log('jon world! user show all user jobs');
   db.Job.findAll({
     include: [db.User],
     where: { UserId: id }
-  }).then(function (data) {
+  }).then(function(data) {
     res.json(data);
     // res.render("../views/user.handlebars", { jobs: data });
   });
 });
 
-router.get('/user/all',function (req, res) {
-  // console.log('jon world!show all users');
+router.get('/user/all',function(req, res) {
   db.User.findAll({
-  }).then(function (data) {
+  }).then(function(data) {
     res.json(data);
   });
 });
+
+router.post('/development',function(req,res) {
+
+  var username = req.body.username;
+  var password = req.body.password;
+  // Check where user
+  db.User.findAll({
+    where: { user_name: username, password: password }
+  })
+  .then(function(hits) {
+    var hits = hits.map(function(hit) {return hit.dataValues});
+    res.json(hits);
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
+
+});
+
+
 module.exports = router;
 
